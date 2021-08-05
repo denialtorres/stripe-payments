bf2caf30a2f155abd6d611a18f676c25fbf01333
Daniel Torres <denial.torres@gmail.com>
2021-08-05 04:47:03 -0500

    Creating Payments with Stripe Elements JS

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,11 +1,35 @@
 class PurchasesController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_product
 
   def show
-    @payment_intent = Stripe::PaymentItent.create(
+    @payment_intent = Stripe::PaymentIntent.create(
       amount: @product.amount,
       currency: 'usd',
     )
+  end
+
+  def create
+    @payment_intent = Stripe::PaymentIntent.retrieve(params[:payment_intent_id])
+    if @payment_intent.status == "succeeded"
+      charge = @payment_intent.charges.data.first
+      card = charge.payment_method_details.card
+
+      Order.create(
+        stripe_id: charge.id,
+        user: current_user,
+        product: @product,
+        card_brand: card.brand.titleize,
+        card_last4: card.last4,
+        card_exp_month: card.exp_month,
+        card_exp_year: card.exp_year
+      )
+
+      redirect_to root_path, notice: "your order has been submitted"
+    else
+      flash[:alert] = "Your order was unsuccessful, plese try again"
+      render action: :show
+    end
   end
 
   private
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,31 +3,85 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-window.Rails = require("@rails/ujs")
-require("@hotwired/turbo-rails")
-require("@rails/activestorage").start()
-require("channels")
+window.Rails = require("@rails/ujs");
+require("@hotwired/turbo-rails");
+require("@rails/activestorage").start();
+require("channels");
 //require("trix")
 //require("@rails/actiontext")
-require("local-time").start()
+require("local-time").start();
 
 // Start Rails UJS
-Rails.start()
+Rails.start();
 
 // Stimulus
-import "controllers"
+import "controllers";
 
 // Bootstrap
-import 'bootstrap'
+import "bootstrap";
 
 document.addEventListener("turbo:load", () => {
-  var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-  var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-    return new bootstrap.Tooltip(tooltipTriggerEl)
-  })
+  const form = document.querySelector("#payment-form");
 
-  var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+  var tooltipTriggerList = [].slice.call(
+    document.querySelectorAll('[data-bs-toggle="tooltip"]')
+  );
+  var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl);
+  });
+
+  var popoverTriggerList = [].slice.call(
+    document.querySelectorAll('[data-bs-toggle="popover"]')
+  );
   var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
-    return new bootstrap.Popover(popoverTriggerEl)
-  })
-})
+    return new bootstrap.Popover(popoverTriggerEl);
+  });
+
+  // Mount Stripe Card Input.
+  if (form == null) {
+    return;
+  }
+  const public_key = document
+    .querySelector("meta[name='stripe-key']")
+    .getAttribute("content");
+  const stripe = Stripe(public_key);
+
+  const elements = stripe.elements();
+
+  const card = elements.create("card");
+
+  card.mount("#card-element");
+
+  card.addEventListener("change", (event) => {
+    var displayError = document.getElementById("card-errors");
+    if (event.error) {
+      displayError.textContent = event.error.message;
+    } else {
+      displayError.textContent = "";
+    }
+  });
+
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    let data = {
+      payment_method: {
+        card: card,
+        billing_details: {
+          name: form.querySelector("#name_on_card").value,
+        },
+      },
+    };
+
+    stripe
+      .confirmCardPayment(form.dataset.paymentIntentId, data)
+      .then((result) => {
+        if (result.error) {
+          var errorElement = document.getElementById("card-errors");
+          errorElement.textContent = result.error.message;
+        } else {
+          form.submit();
+        }
+      });
+  });
+});
